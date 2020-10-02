24efea0830f7dd414c730b95ea073ee3fc0c627e
envy124 <pkgsail.dev@gmail.com>
2020-10-02 12:11:12 +0300

    add deleteProduct & updateProduct

--- src/main/kotlin/com/example/fabricarestcrudexample240/controllers/ProductController.kt
+++ src/main/kotlin/com/example/fabricarestcrudexample240/controllers/ProductController.kt
@@ -1,5 +1,8 @@
 package com.example.fabricarestcrudexample240.controllers
 
+import com.example.fabricarestcrudexample240.dto.DeleteArticleEntityByIdDto
+import com.example.fabricarestcrudexample240.dto.DeleteProductEntityByIdDto
+import com.example.fabricarestcrudexample240.entities.ArticleEntity
 import com.example.fabricarestcrudexample240.entities.ProductEntity
 import com.example.fabricarestcrudexample240.repositories.ProductRepository
 import org.springframework.web.bind.annotation.*
@@ -27,5 +30,15 @@ class ProductController(val products: ProductRepository) {
     @PostMapping("/product")
     fun createProduct(@RequestBody product: ProductEntity): Mono<ProductEntity> {
         return products.save(product)
+    }
+
+    @PutMapping("/product")
+    fun updateProduct(@RequestBody product: ProductEntity): Mono<Int> {
+        return products.updateProduct(product.id, product.title, product.description, product.cost)
+    }
+
+    @DeleteMapping("/product")
+    fun deleteProduct(@RequestBody request: DeleteProductEntityByIdDto): Mono<Void> {
+        return products.deleteById(request.productId)
     }
 }
--- src/main/kotlin/com/example/fabricarestcrudexample240/repositories/ProductRepository.kt
+++ src/main/kotlin/com/example/fabricarestcrudexample240/repositories/ProductRepository.kt
@@ -1,6 +1,13 @@
 package com.example.fabricarestcrudexample240.repositories
 
 import com.example.fabricarestcrudexample240.entities.ProductEntity
+import org.springframework.data.r2dbc.repository.Modifying
+import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import reactor.core.publisher.Mono
 
-interface ProductRepository: ReactiveCrudRepository<ProductEntity, Long>
+interface ProductRepository: ReactiveCrudRepository<ProductEntity, Long> {
+    @Modifying
+    @Query("UPDATE public.product SET title = :title, description = :description, cost = :cost where id = :id")
+    fun updateProduct(id: Long, title: String, description: String, cost: Number): Mono<Int>
+}
